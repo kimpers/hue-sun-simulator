75b5249f1aecbac7e400fb27f59b0622eb58aee6
Kim Persson <kimpers@users.noreply.github.com>
2016-01-24 01:32:18 +0100

    Setup correct testing with mock api, added sunrise

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,6 @@ const nodeHueApi = require('node-hue-api');
 const sunSimulator = require('../lib/hue.js');
 const program = require('commander');
 
-
 program
   .version('0.1.0')
   .command('sunrise <ip> <username> [options...]')
@@ -16,13 +15,14 @@ program
     sunSimulator.sunrise(hueApi, lightState).done(() => console.log('Sunrise simulation successful'),
         (reason) => console.log(`Sunrise simulation failed due to: ${reason}`));
   });
-
+  
+program
+  .command('sunset <ip> <username> [options...]')
+  .action((ip, username, options) => {
+    const hueApi = new nodeHueApi.HueApi(ip, username);
+    const lightState = nodeHueApi.lightState;
+    sunSimulator.sunset(hueApi, lightState).done(() => console.log('Sunset simulation successful'),
+        (reason) => console.log(`Sunset simulation failed due to: ${reason}`));
+  });
 
 program.parse(process.argv);
-
-
-
-
-
-
-
--- test/hue.js
+++ test/hue.js
@@ -1,23 +1,41 @@
 /* jshint esnext: true */
+'use strict'
 const expect = require('chai').expect;
 const hue = require('../lib/hue');
+const mock = require('./mock.js');
 
 
 
-describe("Hue lightning", () => {
-  const mockAPI = {
-    setLightState(id, state){
-      return new Promise((resolve, reject) => {
-        setTimeout(() => resolve());
-      });
-    }
 
-  };
+const sunriseState = [ 'on', 'hue(46920)', 'sat(254)', 'bri(0)', 'on', 'hue(65280)',
+      'bri(128)', 'transition(0)', 'on', 'hue(12750)', 'bri(254)', 'transition(0)',
+      'on', 'sat(0)', 'transition(0)' ];
 
-  
-  it("calls the hue api successfully", () => {
-    // TODO
-    expect(true).to.equal(false);
+describe('sunrise', () => {
+  it('simulates in correct order', (done) => {
+    const lightState = mock.createLightState();
+    hue.sunrise(mock.api, lightState, mock.options).then((results) => {
+      try{
+        expect(results.state).to.eql(sunriseState);
+        done();
+      } catch(e) {
+        done(e);
+      }
+    });
+  });
+});
+
+describe('sunset', () => {
+  it('simulates in correct order', (done) => {
+    const lightState = mock.createLightState();
+    hue.sunset(mock.api, lightState, mock.options).then((results) => {
+      try{
+        expect(results.state).to.eql(sunriseState);
+        done();
+      } catch(e) {
+        done(e);
+      }
+    });
   });
 });
 
