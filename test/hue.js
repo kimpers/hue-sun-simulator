bf51aedf08bc20b805f0aa9e4c7a350e4f4b0af8
Kim Persson <kimpersson88@gmail.com>
2017-10-31 22:58:52 +0100

    UPDATES dependencies, adds linting & cleans up code

--- test/hue.js
+++ test/hue.js
@@ -1,41 +1,23 @@
-/* jshint esnext: true */
+/* global describe, it */
 'use strict'
-const expect = require('chai').expect;
-const hue = require('../lib/hue');
-const mock = require('./mock.js');
-
-
-
+const expect = require('chai').expect
+const hue = require('../lib/hue')
+const mock = require('./mock.js')
 
 const sunriseState = [ 'on', 'hue(46920)', 'sat(254)', 'bri(0)', 'on', 'hue(65280)',
-      'bri(128)', 'transition(0)', 'on', 'hue(12750)', 'bri(254)', 'transition(0)',
-      'on', 'sat(0)', 'transition(0)' ];
+  'bri(128)', 'transition(0)', 'on', 'hue(12750)', 'bri(254)', 'transition(0)',
+  'on', 'sat(0)', 'transition(0)' ]
 
 describe('sunrise', () => {
   it('simulates in correct order', (done) => {
-    const lightState = mock.createLightState();
-    hue.sunrise(mock.api, lightState, mock.options).then((results) => {
-      try{
-        expect(results.state).to.eql(sunriseState);
-        done();
-      } catch(e) {
-        done(e);
+    const lightState = mock.createLightState()
+    hue.sunrise(mock.api, lightState, 1, mock.options).then((results) => {
+      try {
+        expect(results.state).to.eql(sunriseState)
+        done()
+      } catch (e) {
+        done(e)
       }
-    });
-  });
-});
-
-describe('sunset', () => {
-  it('simulates in correct order', (done) => {
-    const lightState = mock.createLightState();
-    hue.sunset(mock.api, lightState, mock.options).then((results) => {
-      try{
-        expect(results.state).to.eql(sunriseState);
-        done();
-      } catch(e) {
-        done(e);
-      }
-    });
-  });
-});
-
+    })
+  })
+})
