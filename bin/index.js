02c36579b9b76d7e9ac41d42971359c8dc5eb13c
Kim Persson <kimpersson88@gmail.com>
2017-10-31 22:58:52 +0100

    UPDATES dependencies, adds linting & cleans up code

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,28 @@
 #! /usr/bin/env node
-'use strict';
+'use strict'
 /* jshint esnext: true */
 
-const nodeHueApi = require('node-hue-api');
-const sunSimulator = require('../lib/hue.js');
-const program = require('commander');
+const nodeHueApi = require('node-hue-api')
+const sunSimulator = require('../lib/hue.js')
+const program = require('commander')
 
 program
   .version('0.1.0')
   .command('sunrise <ip> <username> <lightId> [options...]')
   .action((ip, username, lightId, options) => {
-    const hueApi = new nodeHueApi.HueApi(ip, username);
-    const lightState = nodeHueApi.lightState;
+    const hueApi = new nodeHueApi.HueApi(ip, username)
+    const lightState = nodeHueApi.lightState
     sunSimulator.sunrise(hueApi, lightState, lightId).done(() => console.log('Sunrise simulation successful'),
-        (reason) => console.log(`Sunrise simulation failed due to: ${reason}`));
-  });
-  
+        (reason) => console.log(`Sunrise simulation failed due to: ${reason}`))
+  })
+
 program
   .command('sunset <ip> <username> [options...]')
   .action((ip, username, options) => {
-    const hueApi = new nodeHueApi.HueApi(ip, username);
-    const lightState = nodeHueApi.lightState;
+    const hueApi = new nodeHueApi.HueApi(ip, username)
+    const lightState = nodeHueApi.lightState
     sunSimulator.sunset(hueApi, lightState).done(() => console.log('Sunset simulation successful'),
-        (reason) => console.log(`Sunset simulation failed due to: ${reason}`));
-  });
+        (reason) => console.log(`Sunset simulation failed due to: ${reason}`))
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
--- test/mock.js
+++ test/mock.js
@@ -1,51 +1,50 @@
-const createLightState =  function() {
+const createLightState = function () {
   return {
     state: [],
-    create() {
-      return this;
+    create () {
+      return this
     },
-    on() {
-      this.state.push('on');
-      return this;
-    },
-
-    hue(val) {
-      this.state.push(`hue(${val})`);
-      return this;
+    on () {
+      this.state.push('on')
+      return this
     },
 
-    bri(val) {
-      this.state.push(`bri(${val})`);
-      return this;
+    hue (val) {
+      this.state.push(`hue(${val})`)
+      return this
     },
 
-    sat(val) {
-      this.state.push(`sat(${val})`);
-      return this;
-
+    bri (val) {
+      this.state.push(`bri(${val})`)
+      return this
     },
-    transition(val) {
-      this.state.push(`transition(${val})`);
-      return this;
+
+    sat (val) {
+      this.state.push(`sat(${val})`)
+      return this
+    },
+    transition (val) {
+      this.state.push(`transition(${val})`)
+      return this
     }
-  };
-};
+  }
+}
 
 const api = {
-  setLightState(lightId, state) {
+  setLightState (lightId, state) {
     return new Promise((resolve, reject) => {
-      resolve(state);
-    });
+      resolve(state)
+    })
   }
-};
+}
 
 const options = {
   transitionTime: 0,
   delayOne: 0,
   delayTwo: 0,
   delayThree: 0
-};
+}
 
-exports.createLightState = createLightState;
-exports.api = api;
-exports.options = options;
+exports.createLightState = createLightState
+exports.api = api
+exports.options = options
